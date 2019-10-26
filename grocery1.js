ca02c90ae6c072024334173b54c22e717d14134f
Chidimma <chidimmai@gmail.com>
2019-10-25 23:51:00 -0500

    commit all first

--- grocery1.js
+++ grocery1.js
@@ -1,4 +1,3 @@
-
 // class Grocery1 {
 //   name: string;
 //   quantity: number;
@@ -7,24 +6,19 @@
 //     this.name = theName;
 //     this.quantity = theQuantity;
 //   }
-
-
-
 // }
-
-class Grocery1 {
-  name: string;
-  quantity:number;
-  constructor(name: string, quantity: number) {
-    this.name = name;
-    this.quantity  = quantity;
-}
-}
+var Grocery1 = /** @class */ (function () {
+    function Grocery1(name, quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+    return Grocery1;
+}());
 // There are a couple of ways to init value for arrays, i will show you 2 ways
-let groceries1: Grocery1[] = [
-  {name: 'milk', quantity: 3},
-  {name: 'bread', quantity: 6},
-  {name: 'eggs', quantity: 11}
+var groceries1 = [
+    { name: 'milk', quantity: 3 },
+    { name: 'bread', quantity: 6 },
+    { name: 'eggs', quantity: 11 }
 ];
 // groceries1.push(new Grocery1('milk', 3))
 // function initValueForGroceries1(): void{
@@ -36,28 +30,21 @@ let groceries1: Grocery1[] = [
 //   }
 // }
 // function arraytoString (): string {
-  
 // }
 // initValueForGroceries1();
-function getStrings(groceries1: Grocery1[]) :string  {
-  let str = '';
-  for(let i =0;i < groceries1.length;i++) {
-    let itemString =  'Name: '+groceries1[i].name+' Quantity: ' + groceries1[i].quantity + '----';
-      str += itemString;
-  }
-  return str;
+function getStrings(groceries1) {
+    var str = '';
+    for (var i = 0; i < groceries1.length; i++) {
+        var itemString = 'Name: ' + groceries1[i].name + ' Quantity: ' + groceries1[i].quantity + '----';
+        str += itemString;
+    }
+    return str;
 }
-
-console.log(groceries1)
-
+console.log(groceries1);
 document.body.textContent = getStrings(groceries1);
-
-
-
-
 /*
 1.	Create a file called grocery.ts. It should have a definition of a class with the obvious name Grocery. The class should have some basic attributes such as name, quantity, etc. Feel free to add any other attributes you think will be necessary.
 2.	Add few grocery items to an array of groceries, such as milk, bread, and eggs, along with some quantities (i.e. 3, 6, 11).  Display these grocery items as HTML output.
 3.	The output of this assignment will be grocery.ts and groceries.html displaying the output.
 
-*/
+*/ 
